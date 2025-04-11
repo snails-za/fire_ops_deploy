de1dc0bd563728f899824e62474dddf053215d49
wangju <wangjuchn@outlook.com>
2025-04-11 22:06:49 +0800

    打包脚本分架构

--- save_image-arm.sh
+++ save_image-arm.sh
@@ -3,14 +3,14 @@
 IMAGES=""
 
 echo "pull images"
-for img in $(cat database.yml | grep image: | awk '{print $2}' | uniq | sort);
+for img in $(cat database-arm.yml | grep image: | awk '{print $2}' | uniq | sort);
 do
   echo "pulling image: $img"
   docker pull $img
   IMAGES=${img}" "${IMAGES}
 done
 
-for img in $(cat docker-compose.yml | grep image: | awk '{print $2}' | uniq | sort);
+for img in $(cat docker-compose-arm.yml | grep image: | awk '{print $2}' | uniq | sort);
 do
   echo "pulling image: $img"
   docker pull $img
@@ -19,5 +19,5 @@ done
 
 echo "saving images"
 mkdir -p images
-save_tar_path="images/fastapi_demo_all_image.tar"
+save_tar_path="images/fastapi_demo_all_image_arm.tar"
 docker save -o ${save_tar_path} ${IMAGES} && gzip ${save_tar_path}
